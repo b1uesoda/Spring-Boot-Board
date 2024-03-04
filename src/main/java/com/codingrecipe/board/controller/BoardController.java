a5b7e3f6a4aec209da6d047d39b2bc8186832ffa
b1uesoda <b1uesoda@naver.com>
2024-03-04 10:19:40 +0900

    added postMapping of write

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -2,13 +2,20 @@ package com.codingrecipe.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
     @GetMapping("/write")
-    public String saveForm() {
+    public String writeForm() {
         return "write";
+    }
+
+    @PostMapping("/write")
+    public String save() {
+        return null;
     }
 }
