2a75c803339fc12b2ebceba3f8ef57abf7112f07
b1uesoda <b1uesoda@naver.com>
2024-03-05 22:41:48 +0900

    defined entity column

--- src/main/java/com/codingrecipe/board/entity/BoardEntity.java
+++ src/main/java/com/codingrecipe/board/entity/BoardEntity.java
@@ -1,4 +1,31 @@
 package com.codingrecipe.board.entity;
 // DB의 테이블 역할을 하는 클래스
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "board_table")
 public class BoardEntity {
+    @Id// pk 컬럼 지정. 필수
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(length = 20, nullable = false) // 크기 20, not null
+    private String boardWriter;
+
+    @Column() // 크기 255, null 가능
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    private String boardContents;
+
+    @Column
+    private int boardHits;
 }
